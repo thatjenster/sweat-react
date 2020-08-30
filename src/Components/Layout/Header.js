a739d0c7b52231719c2c99756ac82c309b40c884
Jenny Dinh <jennythi.d@gmail.com>
2020-08-30 18:32:43 -0400

    style

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Header = () => {
     return(
-        <div>Hello</div>
+        <div>
+            
+            <div class="social-box">
+                <div class="social">
+                    <a href="https://www.instagram.com/jennyt.dinh/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://twitter.com/thatjenster" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="https://github.com/thatjenster" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                    <a href="https://www.linkedin.com/in/thatjenster-jennydinh/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
     )
 }
 
