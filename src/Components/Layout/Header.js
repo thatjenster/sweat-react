a471165c3fe2adbd572834b7be36ed0e82e82161
Jenny Dinh <jennythi.d@gmail.com>
2020-09-01 09:50:25 -0400

    add img to header

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import headerImg from './../../Assets/header.png'
 
 const Header = () => {
     return(
         <div>
-            
+            <div className="header-image">
+                <img src={headerImg} alt="header logo"/>
+                <p>Strive for PROGRESS not PERFECTION</p>
+            </div>
+
             <div class="social-box">
                 <div class="social">
                     <a href="https://www.instagram.com/jennyt.dinh/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
