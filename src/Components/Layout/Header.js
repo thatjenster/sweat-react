3b98ec58fe9046c08c1811c1085299a41d2cfcd9
Jenny Dinh <jennythi.d@gmail.com>
2020-09-01 11:37:40 -0400

    style

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
                 <img src={headerImg} alt="header logo"/>
                 <p>Strive for PROGRESS not PERFECTION</p>
             </div>
-
             <div class="social-box">
                 <div class="social">
                     <a href="https://www.instagram.com/jennyt.dinh/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
