138b0bcc7e9acc24c5e7f8b9bde80bea9c346a41
Jenny Dinh <jennythi.d@gmail.com>
2020-09-15 11:28:10 -0400

    Updates

---
+++ precache-manifest.7d7882d45627dac2ef912f39a4d6399a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246ccd04539178b7a87936b5e544b7ce",
+    "url": "/sweat-react/index.html"
+  },
+  {
+    "revision": "c828e25582cdf4879e62",
+    "url": "/sweat-react/static/css/2.4e35ac08.chunk.css"
+  },
+  {
+    "revision": "717665d90fc6d1541312",
+    "url": "/sweat-react/static/css/main.d36fdbe6.chunk.css"
+  },
+  {
+    "revision": "c828e25582cdf4879e62",
+    "url": "/sweat-react/static/js/2.3cd01608.chunk.js"
+  },
+  {
+    "revision": "d56f7d70e49a582c3cb88e84fa36367e",
+    "url": "/sweat-react/static/js/2.3cd01608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717665d90fc6d1541312",
+    "url": "/sweat-react/static/js/main.b8ea7184.chunk.js"
+  },
+  {
+    "revision": "99fd2e0527b35bcabbb0",
+    "url": "/sweat-react/static/js/runtime-main.39ba37a1.js"
+  },
+  {
+    "revision": "7f30b4421cc2215dd898a6dc3d27f562",
+    "url": "/sweat-react/static/media/background.7f30b442.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sweat-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sweat-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sweat-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sweat-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sweat-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bc4f8e7978c365e668fabbce1ca3bc4a",
+    "url": "/sweat-react/static/media/header.bc4f8e79.png"
+  }
+]);
